587493b7c0d8ad70ff8f142e116a2974e91a465c
testhdm <83900038+testhdm@users.noreply.github.com>
2021-07-07 12:15:34 +0530

    Update sample_config.py

--- lycia/sample_config.py
+++ lycia/sample_config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    API_ID = int("api_id")
-    API_HASH = "api_hash"
-    TOKEN = "token"
+    API_ID = int("6469237")
+    API_HASH = "aaaacf752e28564b49714c7ad51fd48b"
+    TOKEN = "1741302430:AAG1F-bSo9a_6D2QiPA24xuQVLfyKJHDSTE"
